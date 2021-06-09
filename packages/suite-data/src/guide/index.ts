a8c5235e93dcc57567f4b84fa1e73d16eaeb3ad8
Jen Tak <jentak@hey.com>
2021-06-09 14:46:36 +0200

    fixup! feat: #3713 Index Guide Content

--- packages/suite-data/src/guide/index.ts
+++ packages/suite-data/src/guide/index.ts
@@ -2,7 +2,7 @@ import { join } from 'path';
 import * as fs from 'fs-extra';
 import simpleGit from 'simple-git';
 import { TMP, GITBOOK_SOURCE, GITBOOK_REVISION, DESTINATION, ASSETS_DIR } from './constants';
-import { parse } from './parser';
+import { Parser } from './parser';
 import { transform } from './transformer';
 
 // See /docs/misc/guide.md for documentation of this script.
@@ -29,7 +29,8 @@ const main = async () => {
     // Fetch content from GitBook mirror.
     await fetchSource(GITBOOK_SOURCE, GITBOOK_REVISION, TMP);
     // Parse content tree.
-    const index = parse();
+    const parser = new Parser(TMP);
+    const index = parser.parse();
 
     pruneDirectory(DESTINATION);
     // Transform the markdown and copy it to the DESTINATION.
